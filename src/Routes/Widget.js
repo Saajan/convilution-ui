9346e658f627273f8c8f5ed53d95ae8c2213271f
Akshay Kulkarni <akulkarni@conviva.com>
2021-12-05 21:35:57 +0530

    Code'

--- src/Routes/Widget.js
+++ src/Routes/Widget.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import http from '../http';
 import ReactECharts from 'echarts-for-react';
+import Widget3 from '../Components/Widget3';
 import { DotsVerticalIcon } from '@heroicons/react/solid'
 
 const getChartOptions_1 = (options) => {
     return {
         xAxis: {
             type: 'category',
-            boundaryGap: false,
+            boundaryGap: true,
             data: ['ExoPlayer', 'HTML5', 'UWPMediaPlayer', 'AVFoundation', 'Roku Scene Graph', 'DSS-HLS']    
         },
         yAxis: {
@@ -35,6 +36,37 @@ const getChartOptions_1 = (options) => {
         ...options,
     }
 }
+const getChartOptions_2 = (options) => {
+    return {
+        xAxis: {
+            type: 'category',
+            boundaryGap: true,
+            data: ['0','0-0.2','0.2-0.4','0.4-0.6', '0.6-0.8', '0.8-1', '1-2', '2-3', '3-4', '4-5', '5-10', '10-50', '50-100']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                type: 'bar',
+                name: 'CIRR %',
+                data: [25, 35, 33, 32.5, 31, 30, 30, 28, {
+                    value: 22,
+                    itemStyle: {
+                      color: '#a90000'
+                    }
+                  }, 22, 18, 15, 12, 4]
+            }
+        ],
+        legend: {
+            // data: 'CIRR %',
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        ...options,
+    }
+}
 const getChartOptions = (widgetData) => {
     const options = {};
 
@@ -44,24 +76,30 @@ const getChartOptions = (widgetData) => {
 
     if (widgetData.name === "My Widget 1") {
         return getChartOptions_1(options);
+    } else if (widgetData.name === "My Widget 2") {
+        return getChartOptions_2(options);
     }
     return options;
 }
 
 function Widget() {
     const { widgetId } = useParams();
-    const [chartData, setChartData] = useState({a: 1});
+    const [chartData, setChartData] = useState({});
     useEffect(() => {
         getChartMeta(widgetId);
     }, [widgetId]);
     const getChartMeta = async () => {
+        setChartData({});
         http.get(`widgets\\${widgetId}`)
          .then((response) => setChartData(response.data))
     }
     return (
         <div className="w-full border border-grey-500 m-2">
             <div>
-                {chartData!== {} && <ReactECharts option={getChartOptions(chartData)} />}
+                {chartData !== {} && chartData.chart !== 'table' && <ReactECharts option={getChartOptions(chartData)} />}
+            </div>
+            <div>
+                {chartData !== {} && chartData.chart === 'table' && <Widget3 />}
             </div>
         </div>
     );
